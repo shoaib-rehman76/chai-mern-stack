1afe5d84669f7b2057a07120ab7ea5caabcd1174
Shoaib <shoaib.rehman@yl-solutions.de>
2024-09-03 16:33:05 +0500

    utils code added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,25 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
 import dotenv from 'dotenv'
 import connectDB from './db/db.js';
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
+const options = {
+    origin: process.env.ORIGIN || '*',
+    credentials: true,
+}
 
 dotenv.config({ path: './.env' })
 
+app.use(cors(options))
+app.use(cookieParser())
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true })) // for understanding the url query params as well [extended] for understanding deep nested objects
+app.use(express.static('public'))
+
 const app = express()
 const PORT = process.env.PORT;
+
 
 connectDB()
 
