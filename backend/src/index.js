257fb8f27320043c803f79ba697342b85c35fcf5
Shoaib <shoaib.rehman@yl-solutions.de>
2024-09-03 15:20:17 +0500

    feature/db connection established

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv'
+import connectDB from './db/db.js';
 
 dotenv.config({ path: './.env' })
-const app = express()
 
+const app = express()
 const PORT = process.env.PORT;
+
+connectDB()
 
 
 app.get('/', (req, res) => {
